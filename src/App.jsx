ef84f2eabf7ebf00d13f08d295a77cedb29c6e65
Julian Witjaksono <151512615+Niervash@users.noreply.github.com>
2024-07-21 21:01:58 +0800

    landing build

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Rumah from './page/home/Home.jsx';
+import Navbar from './navbar/Navbar.jsx';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+    return (
+      <>
+        <Navbar />
+        <Rumah />
+        
+      </>
+    )
+  }
+
+export default App
---
+++ src/page/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Rumah = () => {
+  return (
+    <div>
+      <h1>WELCOME</h1>
+      <btn className="btn btn-warning">like</btn>
+
+    </div>
+  )
+}
+
+export default Rumah
+
