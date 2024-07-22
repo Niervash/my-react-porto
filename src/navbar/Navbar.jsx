3c2f0765a4b7a3f1241f3e83ce95bb8aecf708eb
Julian Witjaksono <151512615+Niervash@users.noreply.github.com>
2024-07-22 10:32:27 +0800

    navbar lese

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './navbar.css'
+import logo from './images/Logo_trasnparent.png'
 
 function Navbar() {
   return (
         // navigation bar start
           <nav className="navbar navbar-expand-lg fixed-top shadow p-3 mb-5 bg-white rounded">
             <div className="container">
-              <a className="navbar-brand me-auto"  href="#">Logo</a>
+              <img src = {logo} alt="Julian_Porto" width="150" height="50" className='logo' href="#"/>
               <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div className="offcanvas-header">
                   <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
@@ -32,7 +33,9 @@ function Navbar() {
                   </ul>
                 </div>
               </div>
-              <a href='#' className='login-button'>Login</a>
+              <a href="https://github.com/Niervash" class="navbar-brand fa-brands fa-github"></a>
+              <a href="https://www.instagram.com/wit_jakk/" class="navbar-brand fa-brands fa-instagram"></a>
+              <a href="https://www.linkedin.com/in/julian-witjaksono-aa706a200/" class="navbar-brand fa-brands fa-linkedin"></a>
               <button className="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
